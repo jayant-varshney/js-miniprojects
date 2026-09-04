0c36bb7cdd56d8e1ffdb058aee0b9119301bdb91
jayant-varshney <iam.jvarshney@gmail.com>
2026-09-04 18:27:48 +0530

    clock completed

--- 3digitalClock/chaiaurcode.js
+++ 3digitalClock/chaiaurcode.js
@@ -1,3 +1,10 @@
 const clock = document.getElementById('clock')
 // or use      
 //const clock = document.queryselector('#clock') //both the lines do the same thing 
+
+
+setInterval(function(){
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
