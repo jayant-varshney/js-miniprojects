0bbb54d6ce8cb49d67d32184b4bfaea8f4ad693c
jayant-varshney <iam.jvarshney@gmail.com>
2026-09-05 13:08:08 +0530

    finished guessthe number

--- 4guessTheNumber/chaiaurcode.js
+++ 4guessTheNumber/chaiaurcode.js
@@ -48,27 +48,53 @@ function validateGuess(guess){
     }
 }
 
-function checkGuess(guess){
-    //
+function checkGuess(guess) {
+    if (guess === randomNumber) {
+        displayMessage(`You guessed it right`);
+        endGame();
+    } 
+    else if (guess < randomNumber) {
+        displayMessage(`Number is TOOO low`);
+    } 
+    else if (guess > randomNumber) {
+        displayMessage(`Number is TOOO High`);
+    }
 }
 
-function displayGuess(guess){
-    //
+function displayGuess(guess) {
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}, `;
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess} `;
 }
 
-function displayMessage(message){
-    //
+function displayMessage(message) {
+    lowOrHi.innerHTML = `<h2>${message}</h2>`;
 }
 
-function endGame(guess){
-    //
+function endGame() {
+    userInput.value = '';
+    userInput.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
 }
 
-function newGame(guess){
-    //
+function newGame() {
+    const newGameButton = document.querySelector('#newGame');
+    newGameButton.addEventListener('click', function (e) {
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess = [];
+        numGuess = 1;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${11 - numGuess} `;
+        userInput.removeAttribute('disabled');
+        startOver.removeChild(p);
+
+        playGame = true;
+    });
+
 }
-
-
-
-
 
